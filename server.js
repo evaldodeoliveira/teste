a0d3be88745c0baf33a52892a21f570c095510fe
evaldodeoliveira <evaldodeoliveirasrs@yahoo.com.br>
2019-07-29 11:53:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/',(req, res) => {
     res.status(200).send({message: 'Hello word'});
 })
+app.get('/ola',(req, res) => {
+    res.status(200).send({message: 'I\'m route ola!'});
+})
 app.listen(3001, () => {
     console.log('API rodando na porta 3001.');
 })
